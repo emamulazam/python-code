622f4eda76477e5c9bfcd95bb1ffbde729244ff7
emamulazam <emamulazam04@gmail.com>
2026-04-07 15:59:38 +0600

    1st

---
+++ 1st.py
@@ -0,0 +1,47 @@
+# print('hi')
+
+# a='hi'
+# b=30
+# b=str(b)
+# print(a+b)
+
+
+# a='shah mohaMmad emamul azam'
+# print(a.title())
+# print(a.upper())
+# print(a.lower())
+
+# print(2+6)
+# print(6-2)
+# print(2*5)
+# print(5**2)
+# print(10/3)
+# print(15%2)
+# print(15//2)
+
+
+# stu=['hi','30']
+# for i in stu:
+#     print(i)
+
+
+# a=[1,2,3,4,5,6,7,8,9]
+# # print(a[3])
+# # b=a[-2]
+# # print(b)
+# print(a)
+# a[3]= 45
+# print(a)
+# b=66
+# a[5]=b
+# print(a)
+
+# a=[1,2,3,4,5,6,7,8,9]
+# print(a)
+# a.append(10)
+# print(a)
+# a.insert(3,15)
+# print(a)
+
+# del a[-2]
+# print(a)
